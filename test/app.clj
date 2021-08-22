915a5b739f516a55f5551ba0b906095f96c9a9bc
zTgx <747674262@qq.com>
2021-08-22 14:40:05 +0800

    feat: add unit test

---
+++ test/app.clj
@@ -0,0 +1,10 @@
+(ns app
+  (:require [clojure.test :refer :all]))
+  
+(deftest addition
+  (is (= 4 (+ 2 2)))
+  (is (= 7 (+ 3 4))))
+
+(deftest subtraction
+  (is (= 1 (- 4 3)))
+  (is (= 3 (- 7 4))))
