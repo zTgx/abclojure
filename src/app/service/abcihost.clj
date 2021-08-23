d7abc6c4ab8965baa1722b18705844dac067737b
zTgx <747674262@qq.com>
2021-08-23 12:51:26 +0800

    docs: add abci api descrition

--- src/app/service/abcihost.clj
+++ src/app/service/abcihost.clj
@@ -4,7 +4,12 @@
 ;;; default port: 26658
 ;;; accept tendermint abci callbacks, including echo/info/check-tx/begin-block/deliver-tx/end-block/commit, etc
 ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ABCI methods are split across four separate ABCI connections:
+;;; - Consensus connection: InitChain, BeginBlock, DeliverTx, EndBlock, Commit
+;;; - Mempool connection: CheckTx
+;;; - Info connection: Info, Query
+;;; - Snapshot connection: ListSnapshots, LoadSnapshotChunk, OfferSnapshot, ApplySnapshotChunk
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (ns app.service.abcihost
   (:require [io.pedestal.http :as http]
@@ -19,7 +24,6 @@
             ;; [com.example.addressbook :as addressbook]
             [tendermint.abci.ABCIApplication.server :as server]
             [tendermint.abci :as abci]))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keep a Tabular route 
@@ -44,12 +48,16 @@
 ;;
 ;; see http://pedestal.io/reference/request-map
 
-(defn mock-hash
-  []
-  (hash "1"))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; init chain
+;;
+;; Usage:
+;;
+;; Called once upon genesis.
+;; If ResponseInitChain.Validators is empty, the initial validator set will be the RequestInitChain.Validators
+;; If ResponseInitChain.Validators is not empty, it will be the initial validator set (regardless of what is in RequestInitChain.Validators).
+;; This allows the app to decide if it wants to accept the initial validator set proposed by tendermint (ie. in the genesis file), or if it wants to use a different one (perhaps computed based on some application specific information in the genesis file).
+;;
 (defn init-chain
   [request-init-chain]
   (ring-resp/response {
@@ -60,12 +68,25 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; echo - info - query
+;;
+;; Usage:
+;;
+;; Echo a string to test an abci client/server implementation
+;;
 (defn echo 
   [request-echo]
   (ring-resp/response {
     :message (:message request-echo)
     }))
 
+;;
+;;Usage:
+;;
+;; Return information about the application state.
+;; Used to sync Tendermint with the application during a handshake that happens on startup.
+;; The returned app_version will be included in the Header of every block.
+;; Tendermint expects last_block_app_hash and last_block_height to be updated during Commit, ensuring that Commit is never called twice for the same block height.
+;;
 (defn info 
   [request-info]
   (ring-resp/response {
@@ -76,6 +97,13 @@
     :last-block-app-hash (byte-array 11)
   }))
     
+;;
+;; Usage:
+;;
+;; Query for data from the application at current or past height.
+;; Optionally return Merkle proof.
+;; Merkle proof includes self-describing type field to support many types of Merkle trees and encoding formats.
+;;
 (defn query 
   []
   (ring-resp/response {
@@ -85,6 +113,16 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; check tx -> mempool
+;;
+;; Usage:
+;;
+;; Technically optional - not involved in processing blocks.
+;; Guardian of the mempool: every node runs CheckTx before letting a transaction into its local mempool.
+;; The transaction may come from an external user or another node
+;; CheckTx need not execute the transaction in full, but rather a light-weight yet stateful validation, like checking signatures and account balances, but not running code in a virtual machine.
+;; Transactions where ResponseCheckTx.Code != 0 will be rejected - they will not be broadcast to other nodes or included in a proposal block.
+;; Tendermint attributes no other value to the response code
+;;
 (defn check-tx 
   []
   (ring-resp/response {
@@ -95,6 +133,14 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; begin -> deliver -> end -> commit
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Usage:
+;;
+;; Signals the beginning of a new block. Called prior to any DeliverTxs.
+;; The header contains the height, timestamp, and more - it exactly matches the Tendermint block header. We may seek to generalize this in the future.
+;; The LastCommitInfo and ByzantineValidators can be used to determine rewards and punishments for the validators. NOTE validators here do not include pubkeys.
+;;
 (defn begin-block
   [request-begin-block]
   ;; (println (str "begin block : " (apply str (map char (:hash request-begin-block)))))
@@ -110,6 +156,13 @@
     } 
   }))
 
+;;
+;;Usage:
+;;
+;; The workhorse of the application - non-optional.
+;; Execute the transaction in full.
+;; ResponseDeliverTx.Code == 0 only if the transaction is fully valid.
+;;
 (defn deliver-tx 
   []
   (ring-resp/response {
@@ -117,6 +170,16 @@
     :info "deliver tx info"
   }))
 
+;;  Usage:
+;;
+;;  Signals the end of a block.
+;;  Called after all transactions, prior to each Commit.
+;;  Validator updates returned by block H impact blocks H+1, H+2, and H+3, but only effects changes on the validator set of H+2:
+;;  H+1: NextValidatorsHash
+;;  H+2: ValidatorsHash (and thus the validator set)
+;;  H+3: LastCommitInfo (ie. the last validator set)
+;;  Consensus params returned for block H apply for block H+1
+;;
 (defn end-block
   []
   (ring-resp/response {
@@ -125,6 +188,17 @@
     ;; :events 
   }))
 
+;;
+;;  Usage:
+;;
+;;  Persist the application state.
+;;  Return an (optional) Merkle root hash of the application state
+;;  ResponseCommit.Data is included as the Header.AppHash in the next block
+;;  it may be empty
+;;  Later calls to Query can return proofs about the application state anchored in this Merkle root hash
+;;  Note developers can return whatever they want here (could be nothing, or a constant string, etc.), so long as it is deterministic - it must not be a function of anything that did not come from the BeginBlock/DeliverTx/EndBlock methods.
+;;  Use RetainHeight with caution! If all nodes in the network remove historical blocks then this data is permanently lost, and no new nodes will be able to join the network and bootstrap. Historical blocks may also be required for other purposes, e.g. auditing, replay of non-persisted heights, light client verification, and so on.
+;;
 (defn commit 
   []
   (ring-resp/response {
@@ -134,6 +208,13 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; snapshot
+
+;;
+;; Usage:
+;;
+;; Used during state sync to discover available snapshots on peers.
+;; See Snapshot data type for details.
+;;
 (defn list-snapshots
   []
   (ring-resp/response {
@@ -146,18 +227,37 @@
     }
   }))
 
+;;
+;; Usage:
+;;
+;; Used during state sync to retrieve snapshot chunks from peers.
+;;
 (defn load-snapshot-chunk
   []
   (ring-resp/response {
     :chunk (byte-array 3)
   }))
 
+;;
+;; Usage:
+;;
+;; OfferSnapshot is called when bootstrapping a node using state sync. The application may accept or reject snapshots as appropriate. Upon accepting, Tendermint will retrieve and apply snapshot chunks via ApplySnapshotChunk. The application may also choose to reject a snapshot in the chunk response, in which case it should be prepared to accept further OfferSnapshot calls.
+;; Only AppHash can be trusted, as it has been verified by the light client. Any other data can be spoofed by adversaries, so applications should employ additional verification schemes to avoid denial-of-service attacks. The verified AppHash is automatically checked against the restored application at the end of snapshot restoration.
+;;
 (defn offer-snapshot
   []
   (ring-resp/response {
     :result 3
   }))
 
+;;
+;; Usage:
+;;
+;; The application can choose to refetch chunks and/or ban P2P peers as appropriate. Tendermint will not do this unless instructed by the application.
+;; The application may want to verify each chunk, e.g. by attaching chunk hashes in Snapshot.Metadata and/or incrementally verifying contents against AppHash.
+;; When all chunks have been accepted, Tendermint will make an ABCI Info call to verify that LastBlockAppHash and LastBlockHeight matches the expected values, and record the AppVersion in the node state. It then switches to fast sync or consensus and joins the network.
+;; If Tendermint is unable to retrieve the next chunk after some time (e.g. because no suitable peers are available), it will reject the snapshot and try a different one via OfferSnapshot. The application should be prepared to reset and accept it or abort as appropriate.
+;;
 (defn apply-snapshot-chunk
   []
   (ring-resp/response {
@@ -165,6 +265,12 @@
   }))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; flush
+
+;;
+;; Usage:
+;;
+;; Signals that messages queued on the client should be flushed to the server. It is called periodically by the client implementation to ensure asynchronous requests are actually sent, and is called immediately to make a synchronous request, which returns when the Flush response comes back.
+;;
 (defn abci-flush
   []
   (println "flushed, no return."))
